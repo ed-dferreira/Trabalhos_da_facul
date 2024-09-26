be42a522523620b70f285b8c35b0756764543114
Ed ubuntu <eduardoodferreiradev@gmail.com>
2024-09-26 15:48:36 -0300

    Adicionando o primeiro trabalho de poo

---
+++ projeto/Semestre1_2/estudantes/entidades/Produto.java
@@ -0,0 +1,21 @@
+package estudantes.entidades;
+
+/**
+ * Classe que define um produto no supermercado.
+ * <br><br>
+ * Essa classe pode ser estendida. Adicionalmente, ela deve:
+ * <br><br>
+ * 1) ter um construtor completo,<br>
+ * 2) implementar métodos de acesso (getters), sendo que métodos de modificação
+ * (setters) NÃO devem ser implementados,<br>
+ * 3) sobre-escrever os métodos toString, equals e hashCode,<br>
+ * 4) ter um JavaDoc útil e completo.
+ * <br><br>
+ * <strong>Não devem ser criados métodos adicionais nessa classe.</strong>
+ * 
+ * @author Jean Cheiran
+ * @version 1.0
+ */
+public class Produto {
+    
+}
