26480cbb8b0a155fba1aebfbd3576d024beb53b6
Ed <eduardoodferreiradev@gmail.com>
2024-11-20 10:53:20 -0300

    Logica

---
+++ pegaPrea/src/main/java/Logica/JogoMainLogica.java
@@ -0,0 +1,8 @@
+package Logica;
+
+public class JogoMainLogica {
+    public static void main(String[] args) {
+        JogoLogica jogo = new JogoLogica();
+        jogo.iniciar();
+    }
+}
---
+++ pegaPrea/src/main/java/Logica/PosicaoLogica.java
@@ -0,0 +1,10 @@
+package Logica;
+
+public class PosicaoLogica {
+    public int linha, coluna;
+
+    public PosicaoLogica(int linha, int coluna) {
+        this.linha = linha;
+        this.coluna = coluna;
+    }
+}
